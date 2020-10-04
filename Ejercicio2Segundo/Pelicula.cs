ebe6e047d2e386f55f0e0f242824b016934f88bb
Andrés Sánchez <anfurtivo1@hotmail.com>
2020-10-04 23:44:59 +0200

    Se ha añadido la parte de añadir peliculas y mirar el listado de las peliculas que se han hecho y esta casi el poder modificarlas.

--- Ejercicio2Segundo/Pelicula.cs
+++ Ejercicio2Segundo/Pelicula.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,11 +7,33 @@ namespace Ejercicio2Segundo
 {
     class Pelicula
     {
-        String Titulo;
-        String Director;
+        String titulo;
+        String director;
         int listadoActores;
 
-        
+        public string Titulo { get => titulo; set => titulo = value; }
+        public string Director { get => director; set => director = value; }
+        public int ListadoActores { get => listadoActores; set => listadoActores = value; }
+
+        public Pelicula(String titulo,String director,int listadoActores)
+        {
+            this.Titulo = titulo;
+            this.Director = director;
+            this.ListadoActores = listadoActores;
+        }
+
+        public Pelicula()
+        {
+
+        }
+
+
+
+        public String mostrarInfo()
+        {
+            return "Titulo: " + titulo+" Director:"+director+" listado de actores: "+listadoActores;
+
+        }
 
     }
 }
--- Ejercicio2Segundo/Program.cs
+++ Ejercicio2Segundo/Program.cs
@@ -28,8 +28,12 @@ namespace Ejercicio2Segundo
 
                     case 2:
 
-                        ArrayList peliculas;
+                        ArrayList peliculas = new ArrayList();
                         int opcionP;
+                        Pelicula pelicula;
+                        String titulo;
+                        String director;
+                        int listadoActores;
                         do
                         {
                             Console.WriteLine("\tElige una opcion");
@@ -44,29 +48,71 @@ namespace Ejercicio2Segundo
                             switch (opcionP)
                             {
                                 case 1:
+                                    Console.WriteLine("\tIndica el titulo");
+                                    titulo = Console.ReadLine();
+                                    Console.WriteLine("\tIndica el director");
+                                    director = Console.ReadLine();
+                                    Console.WriteLine("\tIndica la cantidad de actores");
+                                    listadoActores = int.Parse(Console.ReadLine());
 
-                                break;
-
-                                case 2:
+                                    pelicula = new Pelicula(titulo,director,listadoActores);
+                                    peliculas.Add(pelicula);
 
                                     break;
 
-                                case 3:
+                                case 2:
+                                    Pelicula pelicula1;
+                                    Console.WriteLine("Estas son todas las peliculas que se han añadido");
+                                    for (int i = 0; i < peliculas.Count; i++)
+                                    {
+                                        pelicula1 = (Pelicula)peliculas[i];
+                                        Console.WriteLine(pelicula1.mostrarInfo());
+                                    }
+                                    
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                    break;
 
+                                case 3:
+                                    Pelicula pelicula2;
+                                    String nombrePeli="";
+                                    String tituloN="";
+                                    String directorN="";
+                                    int listadoAN=1;
+                                    Console.WriteLine("Estas son todas las peliculas que se han añadido");
+                                    for (int i = 0; i < peliculas.Count; i++)
+                                    {
+                                        pelicula2 = (Pelicula)peliculas[i];
+                                        if (pelicula2.Titulo.Equals(nombrePeli))
+                                        {
+                                            pelicula2.Director = directorN;
+                                            pelicula2.Titulo = tituloN;
+                                            pelicula2.ListadoActores = listadoAN;
+                                        }
+                                    }
+
+                                    Console.ReadKey();
+                                    Console.Clear();
                                     break;
 
                                 case 4:
 
+                                    Console.ReadKey();
+                                    Console.Clear();
                                     break;
 
                                 case 5:
-
+                                    Console.WriteLine("\tSe acabaron las peliculas");
+                                    Console.ReadKey();
+                                    Console.Clear();
                                     break;
                                 default:
+                                    Console.ReadKey();
+                                    Console.Clear();
                                     break;
                             }
 
-                        } while (true);
+                        } while (opcionP!=5);
 
                         
 
